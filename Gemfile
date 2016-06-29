35970f5b9ad10b0161df6b3b45384013892f62ec
Yuhei Urabe <Yuhei Urabe>
2016-06-29 17:11:30 +0900

    Create detail page

--- Gemfile
+++ Gemfile
@@ -48,4 +48,5 @@ gem 'pry-rails'
 gem 'compass-rails','~> 2.0'
 gem 'sprockets', '2.11.0'
 gem 'devise'
+gem 'kaminari'
 
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,11 @@
 class TweetsController < ApplicationController
 
   def index
-    @tweets = Tweet.all.order("created_at DESC")
+    @tweets = Tweet.all.page(params[:page]).per(5).order("created_at DESC")
   end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
 end
